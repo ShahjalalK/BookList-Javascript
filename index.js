4487c8b4db07e948b15c7f7331690df19b08da73
ShahjalalK <83275358+ShahjalalK@users.noreply.github.com>
2021-09-14 10:35:11 +0600

    Update index.js
    change final project

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const formId = document.querySelector('#formId')
 const title = document.querySelector('#title')
 const author = document.querySelector('#author')
@@ -6,56 +5,51 @@ const email = document.querySelector('#email')
 const bookList = document.querySelector('#book-list')
 const container = document.querySelector('.container')
 
-class Book {
-    constructor(title, author, email) {
+// Books Patern
+class Book{
+    constructor(title, author, email){
         this.title = title,
-            this.author = author,
-            this.email = email
+        this.author = author,
+        this.email = email
     }
+
 }
-
-class UI {
-    static displayBooks() {
-      const Books = Store.getBooks()
-      Books.forEach((book) => UI.addToListBooks(book)) 
+class UI{
+    static bookDisplay(){
+        const books = Store.getBooks()
+        books.forEach((book) => UI.addToBookList(book))
     }
-    static addToListBooks(books) {
-        const row = document.createElement('tr')
-        row.innerHTML = `
-            <td>${books.title}</td>
-            <td>${books.author}</td>
-            <td>${books.email}</td>
-            <td><i class="fas fa-times delete"></i></td>
-        `
-        bookList.appendChild(row)
-
+    static addToBookList(books){
+      const row = document.createElement('tr') 
+      row.innerHTML = `
+        <td>${books.title}</td>
+        <td>${books.author}</td>
+        <td>${books.email}</td>
+        <td><i class="fas fa-times delete"></i></td>
+      ` 
+      bookList.appendChild(row)
     }
-    static alert(massage, className){
+    static alert(content, className){
         const div = document.createElement('div')
         div.className = `alert alert-${className}`
-        div.appendChild(document.createTextNode(massage))
+        div.appendChild(document.createTextNode(content))
         container.insertBefore(div, formId)
         setTimeout(() => {
             const alert = document.querySelector('.alert')
             alert.remove()
         }, 2000)
     }
-    static clearForm(){
-        title.value = ''  
-        author.value = ''  
-        email.value = ''  
-    }
-    static removeItem(el){
+    static bookListRemove(el){
         if(el.classList.contains('delete')){
             el.parentElement.parentElement.remove()
         }
     }
+    static clearForm(){
+        title.value = ''
+        author.value = ''
+        email.value = ''
+    }
 }
-
-document.addEventListener('DOMContentLoaded', UI.displayBooks)
-
-
-// Storage Books
 
 class Store{
     static getBooks(){
@@ -67,50 +61,45 @@ class Store{
         }
         return books
     }
-    static addStoreBook(book){
-        const booksed = Store.getBooks()
-        booksed.push(book)
-        localStorage.setItem('books', JSON.stringify(booksed))
-    }
-    static removeItems(email){
+    static addBooks(book){
         const books = Store.getBooks()
-        books.forEach((book, index) => {            
-          if(book.email === email){
-            books.splice(index, 1)
-          }
-           
+        books.push(book)
+        localStorage.setItem('books', JSON.stringify(books))
+    }
+    static removeBooksItem(email){
+        const books = Store.getBooks()
+        books.forEach((book, index) => {
+            if(book.email === email){
+                books.splice(index, 1)
+            }
         })
         localStorage.setItem('books', JSON.stringify(books))
     }
 }
 
-
+document.addEventListener('DOMContentLoaded', UI.bookDisplay)
 
 formId.addEventListener('submit', (e) => {
     e.preventDefault()
     const titleValue = title.value
     const authorValue = author.value
     const emailValue = email.value
-    if (titleValue && authorValue && emailValue) {
+    if(titleValue && authorValue && emailValue){
         const books = new Book(titleValue, authorValue, emailValue)
-        UI.addToListBooks(books)
-       Store.addStoreBook(books)       
-        UI.alert('Book List Success', 'success')
-        UI.clearForm()
+    UI.addToBookList(books)
+    Store.addBooks(books)
+    UI.alert('Please full fill item', 'success')
+    UI.clearForm()
     }else{
-        UI.alert('Please full fill up', 'danger')
+        UI.alert('Please full fill item', 'danger')
     }
+    
 })
 
 bookList.addEventListener('click', (e) => {
     const el = e.target
-    UI.removeItem(el)
-    Store.removeItems(e.target.parentElement.previousElementSibling.textContent)
-    UI.alert('Item Remove', 'warning')
+    UI.bookListRemove(el)
+    Store.removeBooksItem(e.target.parentElement.previousElementSibling.textContent)
+    
+    UI.alert('Book list remove', 'info')
 })
-
-
-
-
-
-
